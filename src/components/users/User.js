3ec326bf0dcfe27eb58550643146fda861ae1f66
Eric Moore <ericdmoore@cmail.carleton.ca>
2020-09-09 21:11:32 -0400

    added user.js routes and pages added

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -3,10 +3,25 @@ import { Link } from "react-router-dom";
 
 export class User extends Component {
   componentDidMount() {
+    this.setState({
+      loading: true,
+    });
     const username = this.props.match.params.login;
-    console.log(username);
-    console.log(this.props.getUser(username));
+    // console.log(username);
+    // console.log(this.props.getUser(username));
+    this.props.getUser(username);
     // this.props.getUser(this.props.match.params.login); //pull username out of url
+    this.setState({
+      loading: false,
+    });
+  }
+
+  checkHireable(status) {
+    if (status) {
+      return <i className="fa fa-check text-success"> Hireable</i>;
+    } else {
+      return <i className="fa fa-times-circle text-danger"> Hireable</i>;
+    }
   }
 
   render() {
@@ -18,6 +33,7 @@ export class User extends Component {
       blog,
       login,
       html_url,
+      company,
       followers,
       following,
       public_repos,
@@ -32,6 +48,63 @@ export class User extends Component {
         <Link to="/home" className="btn btn-light">
           Back to search
         </Link>
+        {this.checkHireable(hireable)}
+        <div className="card grid-2">
+          <div className="all-center">
+            <img
+              src={avatar_url}
+              className="round-img"
+              alt="User avatar"
+              style={{ width: "150px" }}
+            />
+            <h2>{name}</h2>
+            <h4>Location: {location}</h4>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>{bio}</h3>
+              </Fragment>
+            )}
+            <a href={html_url} className="btn btn-dark my-1">
+              GitHub Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong>
+                    {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company: </strong>
+                    {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong>
+                    {blog}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {public_repos && (
+                  <Fragment>
+                    <strong>Public Repositories: </strong>
+                    {public_repos}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
       </Fragment>
     );
   }
